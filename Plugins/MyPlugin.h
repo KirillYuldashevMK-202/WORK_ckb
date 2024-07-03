4b08da66036e2d00e9edd1fc336799872a40dfec
KirillYuldashevMK-202 <154894714+KirillYuldashevMK-202@users.noreply.github.com>
2024-07-03 17:14:54 +0500

    Update MyPlugin.h

--- Plugins/MyPlugin.h
+++ Plugins/MyPlugin.h
@@ -1,1 +1,17 @@
+#ifndef MY_PLUGIN_H
+#define MY_PLUGIN_H
+
+class InterfacePlugin {
+public:
+    virtual ~InterfacePlugin() {}
+    virtual void InitPlugin() = 0;
+    virtual void WorkPlugin() = 0;
+};
+
+extern "C" {
+    InterfacePlugin* CreatePlugin();
+    void DeletePlugin(InterfacePlugin* plugin);
+}
+
+#endif
 
