8d7f462e3c7d952f6226750d67ed4993da55f65d
KirillYuldashevMK-202 <154894714+KirillYuldashevMK-202@users.noreply.github.com>
2024-07-04 09:43:27 +0500

    Update main.cpp

--- Plugins/main.cpp
+++ Plugins/main.cpp
@@ -1,23 +1,48 @@
 #include "MyPlugin.h"
 #include <iostream>
 #include <Windows.h>
+#include <fstream>
+#include <vector>
+
+typedef InterfacePlugin* (*CreatePluginFunc)();
+typedef void (*DeletePluginFunc)(InterfacePlugin*);
 
 int main() {
-	HMODULE MyPlugins = LoadLibraryA("PluginMsgbox.dll");
-	if (!MyPlugins) {
-		std::cout << "Error: Plugin not loaded" << std::endl;
-		exit(1);
+	std::string pluginsDirectory = "Myplugin/";
+	std::vector<std::string> pluginFiles;
+	pluginFiles.push_back(pluginsDirectory + "PluginTest.dll");
+	pluginFiles.push_back(pluginsDirectory + "PluginMsgbox.dll");
+	for (int i = 0; i < pluginFiles.size();i+=1) {
+		HMODULE MyPlugins = LoadLibraryA(pluginFiles[i].c_str());
+		if (!MyPlugins) {
+			std::cerr << "Error DLL: " << pluginFiles[i] << std::endl;
+			continue;
+		}
+		CreatePluginFunc CreatePlugin = nullptr;
+		if (pluginFiles[i] == pluginsDirectory + "PluginMsgbox.dll") {
+			CreatePluginFunc CreatePlugin = (CreatePluginFunc)GetProcAddress(MyPlugins, "CreatePluginMsgbox");
+		}
+		else if (pluginFiles[i] == pluginsDirectory + "PluginTest.dll") {
+			CreatePluginFunc CreatePlugin = (CreatePluginFunc)GetProcAddress(MyPlugins, "CreatePluginTest");
+		}
+		InterfacePlugin* Plugin = CreatePlugin();
+		if (!Plugin) {
+			std::cout << "Error: Plugin " << pluginFiles[i] << " not create" << std::endl;
+			continue;
+		}
+		Plugin->InitPlugin();
+		Plugin->WorkPlugin();
+
+		DeletePluginFunc DeletePlugin = nullptr;
+		if (pluginFiles[i] == "PluginMsgbox.dll") {
+			DeletePluginFunc DeletePlugin = (DeletePluginFunc)GetProcAddress(MyPlugins, "DeletePluginMsgbox");
+		}
+		else if (pluginFiles[i] == "PluginTest.dll") {
+			DeletePluginFunc DeletePlugin = (DeletePluginFunc)GetProcAddress(MyPlugins, "DeletePluginTest");
+		}
+		DeletePlugin(Plugin);
 	}
 
-	InterfacePlugin* MsgPlugin = CreatePlugin();
-	if (!MsgPlugin) {
-		std::cout << "Error: Plugin not create" << std::endl;
-		FreeLibrary(MyPlugins);
-		return 1;
-	}
-
-	MsgPlugin->InitPlugin();
-	MsgPlugin->WorkPlugin();
-	DeletePlugin(MsgPlugin);
+	
 	return 0;
 }
