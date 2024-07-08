2f6fdcb37a137e20be45d60094e17726bf8b03d1
KirillYuldashevMK-202 <154894714+KirillYuldashevMK-202@users.noreply.github.com>
2024-07-08 15:52:10 +0500

    Create MyPlugin.h

---
+++ ScanPortLinWin/MyPlugin.h
@@ -0,0 +1,14 @@
+#ifndef MY_PLUGIN_H
+#define MY_PLUGIN_H
+
+class InterfacePlugin {
+public:
+    virtual void InitPlugin() = 0;
+    virtual void WorkPlugin(int s,int e) = 0;
+};
+
+extern "C" InterfacePlugin* CreatePlugin();
+
+extern "C" void DeletePlugin(InterfacePlugin * plugin);
+
+#endif
