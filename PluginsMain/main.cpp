6e7eb35076034a53111e768e8ce4d251bf2132ad
KirillYuldashevMK-202 <154894714+KirillYuldashevMK-202@users.noreply.github.com>
2024-07-08 10:54:05 +0500

    Update main.cpp

--- PluginsMain/main.cpp
+++ PluginsMain/main.cpp
@@ -5,26 +5,9 @@
 #include <fstream>
 #include <vector>
 
-#ifdef _WIN32
-	#include <Windows.h>
-#else
-	#include <dlfcn.h>
-#endif
-
-#ifdef _WIN32
-	typedef InterfacePlugin* (__cdecl* MYPROCcreate)();
-	typedef void(__cdecl* MYPROCdelete)(InterfacePlugin*);
-#else
-	typedef InterfacePlugin* (*MYPROCcreate)();
-	typedef void* (*MYPROCdelete)(InterfacePlugin*);
-#endif
-
 int main() {
-#ifdef _WIN32
-	loaderPlugins MainPlugin("C:/Users/Kirill/source/Work1/newLinux/Myplugin/");
-#else
+	//loaderPlugins MainPlugin("C:/Users/Kirill/source/Work1/newLinux/Myplugin/");
 	loaderPlugins MainPlugin("/home/kirill/workckb/");
-#endif
 	MainPlugin.loadPlugin();
 	MainPlugin.unloadPlugin();
 	MainPlugin.executePlugin();
