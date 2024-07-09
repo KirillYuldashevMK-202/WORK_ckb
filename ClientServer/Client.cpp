0f4102c68a2e5e2dc573d1bdae8e40c67442de06
KirillYuldashevMK-202 <154894714+KirillYuldashevMK-202@users.noreply.github.com>
2024-07-09 15:40:08 +0500

    Update Client.cpp

--- ClientServer/Client.cpp
+++ ClientServer/Client.cpp
@@ -2,27 +2,77 @@
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 #include <fstream>
+#include <string>
+
+#define SIZEBUF 1024
 
 #pragma comment(lib, "Ws2_32.lib")
 
-void UploadFile(SOCKET sock, const std::string file,int sizeFile) {
-    std::ifstream FileUpload;
-
-    FileUpload.open(file);
-    if (!(FileUpload.is_open())) {
-        std::cout<<"Error: open file upload"<<std::endl;
-        return;
+void UploadFile(SOCKET sock, const std::string file) {
+    //Отправка команды и имени файла
+    std::string command = "Upload " + file + ' ';
+    send(sock, command.c_str(), command.size(), 0);
+    char DataFileUpload[SIZEBUF] = { 0 };
+    std::ifstream FileUpload(file, std::ios::binary);
+    //Загрузка информации
+    while (FileUpload.read(DataFileUpload, SIZEBUF) || FileUpload.gcount() > 0) {
+        int bytesToSend = FileUpload.gcount();
+        int bytesSent = 0;
+        while (bytesSent < bytesToSend) {
+            int sent = send(sock, DataFileUpload + bytesSent, bytesToSend - bytesSent, 0);
+            if (sent == -1) {
+                FileUpload.close();
+                break;
+            }
+            bytesSent += sent;
+        }
     }
+    shutdown(sock, 1);
+}
 
-    std::string command = "Upload " + file + " ";
+//Загрузка файла на сервер
+void UnloadFile(SOCKET sock) {
+    //Команда для сервера для загрузки
+    std::string command = "Unload ";
     send(sock, command.c_str(), command.size(), 0);
 
-    char* buffer = new char[sizeFile];
-    FileUpload.get(buffer, sizeFile);
-    std::string Datafile = std::string(buffer);
-    send(sock, Datafile.c_str(), Datafile.size(), 0);
+    //Буфер для сообщений между сервером и клиентом
+    char Msgserver[SIZEBUF] = { 0 };
 
-    delete[] buffer;
+    //Считывания имен файлов от сервера
+    memset(Msgserver, 0, SIZEBUF);
+    recv(sock, Msgserver, SIZEBUF, 0);
+    std::cout << Msgserver << std::endl;
+
+    //Считывание и отправка имени файла для выгрузки
+    std::string FileUnload;
+    std::cout << "Enter the file name to unload: ";
+    std::getline(std::cin, FileUnload);
+    send(sock, FileUnload.c_str(), FileUnload.size(), 0);
+
+    //Сообщение об открытии файла
+    memset(Msgserver, 0, SIZEBUF);
+    recv(sock, Msgserver, SIZEBUF, 0);
+    std::cout << Msgserver << std::endl;
+
+    //Создание и запись информации с файла сервера
+    char DataFileUnload[SIZEBUF];
+    int bytesRead;
+    FileUnload.erase(0, 2);
+    std::ofstream newFile(FileUnload);
+    if (!newFile.is_open()) {
+        std::cerr << "Failed to create file" << std::endl;
+        return;
+    }
+    while ((bytesRead = recv(sock, DataFileUnload, SIZEBUF, 0)) > 0) {
+        newFile.write(DataFileUnload, bytesRead);
+    }
+    newFile.close();
+}
+
+void Exit(SOCKET sock) {
+    std::string command = "Exit ";
+    send(sock, command.c_str(), command.size(), 0);
 }
 
 int main() {
@@ -58,8 +108,9 @@ int main() {
 
     ServerInfo.sin_port = htons(27015);
     Status = connect(ServSock, (sockaddr*)&ServerInfo, sizeof(ServerInfo));
-    UploadFile(ServSock, "newnewTest.txt", 1024);
-
+    UploadFile(ServSock, "Test.txt");
+    //UnloadFile(ServSock);
+    //Exit(ServSock);
     closesocket(ServSock);
     WSACleanup();
     return 0;
