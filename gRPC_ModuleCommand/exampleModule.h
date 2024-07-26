0d0518c8714061aa77229008b39a8e4090ae61ad
KirillYuldashevMK-202 <154894714+KirillYuldashevMK-202@users.noreply.github.com>
2024-07-26 10:01:18 +0500

    Update exampleModule.h

--- gRPC_ModuleCommand/exampleModule.h
+++ gRPC_ModuleCommand/exampleModule.h
@@ -1,9 +1,9 @@
 #ifndef FILES_MODULE
 #define FILES_MODULE
 
-
 #include "InterfaceModule.h"
 #include <filesystem>
+#include <thread>
 #include <string>
 
 class Files : public Module1 {
@@ -20,6 +20,9 @@ public:
 		else {
 			return "Unknown";
 		}
+	}
+	void StartModule(const std::string& command, std::function<void(const std::string&)> callback) override {
+		std::thread([this, command, callback]() { std::string Res = Command(command); callback(Res); } ).detach();	
 	}
 };
 
